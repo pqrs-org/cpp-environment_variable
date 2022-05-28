0e6df88cc4c26af38a82c252737e6e25ac873f41
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-28 14:26:24 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,9 +1,14 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/environment_variable.hpp>
 
-TEST_CASE("find") {
-  REQUIRE(pqrs::environment_variable::find("PATH"));
-  REQUIRE(pqrs::environment_variable::find("UNKNOWN_ENVIRONMENT_VARIABLE") == std::nullopt);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "find"_test = [] {
+    expect(pqrs::environment_variable::find("PATH") != std::nullopt);
+    expect(pqrs::environment_variable::find("UNKNOWN_ENVIRONMENT_VARIABLE") == std::nullopt);
+  };
+
+  return 0;
 }
