4358b8c86e827661ace7a7f9934fa6d4bf78e3ee
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-17 20:50:38 +0900

    Add parser.hpp

--- include/pqrs/environment_variable.hpp
+++ include/pqrs/environment_variable.hpp
@@ -4,19 +4,22 @@
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
+#include "environment_variable/parser.hpp"
 #include <cstdlib>
 #include <optional>
 #include <string>
 
 namespace pqrs {
 namespace environment_variable {
+
 inline std::optional<std::string> find(const std::string& name) {
   if (const char* p = std::getenv(name.c_str())) {
     return p;
   }
   return std::nullopt;
 }
+
 } // namespace environment_variable
 } // namespace pqrs
