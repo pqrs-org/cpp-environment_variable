4e13376cd5a4fa0d37d8a524a39717be2031c3cf
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-18 08:34:50 +0900

    Version 1.1.0

--- include/pqrs/environment_variable.hpp
+++ include/pqrs/environment_variable.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::environment_variable v1.0
+// pqrs::environment_variable v1.1
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
